0d4ea659ffa2d5b0be5e90c9bf8d81d084dcf694
hitalin <hitalin@proton.me>
2024-09-08 05:55:45 +0900

    update: simplify the title in profile

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,10 +1,10 @@
-import './globals.css'
-import type { Metadata } from "next";
+import './globals.css';
+import type { Metadata } from 'next';
 import { Providers } from './providers';
 
 export const metadata: Metadata = {
-  title: "ひたりん(@hitalin)",
-  description: "Medicine cures vulnerability, but vulnerability is the essence of humanity.",
+  title: 'ひたりん',
+  description: 'Medicine cures vulnerability, but vulnerability is the essence of humanity.',
 };
 
 export default function RootLayout({
@@ -13,7 +13,7 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="en">
+    <html lang='en'>
       <body>
         <Providers>{children}</Providers>
       </body>
